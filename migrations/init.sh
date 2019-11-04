ab01c26b8b646842aef8d7b44cc8115080dc26aa
Mason Mullins <mmullins@localadmins-MacBook-Pro.local>
2019-11-04 10:15:54 -0500

    Adding database init script

---
+++ migrations/init.sh
@@ -0,0 +1,47 @@
+#!/bin/sh
+
+# create database 
+docker run -it --link mariadb:mysql --rm mariadb sh -c 'exec mysql -h"$MYSQL_PORT_3306_TCP_ADDR" -P"$MYSQL_PORT_3306_TCP_PORT" -uroot -p"$MYSQL_ENV_MYSQL_ROOT_PASSWORD" -e "CREATE OR REPLACE DATABASE approximately_normal"'
+
+# create calendar table
+docker run -it --link mariadb:mysql --rm mariadb sh -c 'exec mysql -h"$MYSQL_PORT_3306_TCP_ADDR" -P"$MYSQL_PORT_3306_TCP_PORT" -uroot -p"$MYSQL_ENV_MYSQL_ROOT_PASSWORD" -e "
+USE approximately_normal;
+CREATE OR REPLACE TABLE calendar (
+	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	name VARCHAR(255) NULL,
+	start_date DATE NULL,
+	end_date DATE NULL,
+	event_id INT NOT NULL,
+	created_at DATE NULL,
+	created_by VARCHAR(255) NULL,
+	udpated_at DATE NULL,
+	updated_by VARCHAR(255) NULL
+);"'
+
+# create event table
+docker run -it --link mariadb:mysql --rm mariadb sh -c 'exec mysql -h"$MYSQL_PORT_3306_TCP_ADDR" -P"$MYSQL_PORT_3306_TCP_PORT" -uroot -p"$MYSQL_ENV_MYSQL_ROOT_PASSWORD" -e "
+USE approximately_normal;
+CREATE OR REPLACE TABLE event (
+	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	name VARCHAR(255) NULL,
+	description VARCHAR(255) NULL,
+	location VARCHAR(255) NULL,
+	created_at DATE NULL,
+	created_by VARCHAR(255) NULL,
+	udpated_at DATE NULL,
+	updated_by VARCHAR(255) NULL
+);"'
+
+# create the member table
+docker run -it --link mariadb:mysql --rm mariadb sh -c 'exec mysql -h"$MYSQL_PORT_3306_TCP_ADDR" -P"$MYSQL_PORT_3306_TCP_PORT" -uroot -p"$MYSQL_ENV_MYSQL_ROOT_PASSWORD" -e "
+USE approximately_normal;
+CREATE OR REPLACE TABLE member (
+	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	first_name VARCHAR(255) NOT NULL,
+	last_name VARCHAR(255) NOT NULL,
+	email VARCHAR(255) NOT NULL,
+	created_at DATE NULL,
+	created_by VARCHAR(255) NULL,
+	udpated_at DATE NULL,
+	updated_by VARCHAR(255) NULL
+);"'
